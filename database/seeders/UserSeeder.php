2bf1f28a59c83e3fbaf7643741f41bdebc2508d4
MaweedDV <desarrollopuertomontt@gmail.com>
2025-03-19 10:43:21 -0300

    SUBIDA DE PROYECTO

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,6 +12,19 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create([
+            'rut' => '18935579-3',
+            'nombre' => 'Manuel Jesus',
+            'apellido_paterno' => 'Gallardo',
+            'apellido_materno' => 'Pavez',
+            'calidad_juridica' => '1',
+            'email' => 'manuel.gallardo@puertomontt.cl',
+            'position' => 'Administrador de Sistema',
+            'role' => 'admin',
+            'password' => bcrypt('123456789'),
+            'admission_date' => '2020-01-01',
+            'end_date' => '2030-01-01',
+            'active' => 1
+        ]);
     }
 }
